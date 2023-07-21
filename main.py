8c34215b82eb7dd496bcfe08517c78cb3880c5c8
Marcel Suter <marcel.suter@bzz.ch>
2023-07-21 09:39:30 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def main_condition():
+    # Write your program with a condition here and remove the line "pass"
+    pass
+
+
+def main_infinite():
+    # Write your program with break here and remove the line "pass"
+    pass
+
+
+if __name__ == '__main__':
+    main_condition()
+    main_infinite()
---
+++ main_test.py
@@ -0,0 +1,32 @@
+from main import main_condition, main_infinite
+
+
+def test_condition_1(capsys, monkeypatch):
+    inputs = iter(['no'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main_condition()
+    captured = capsys.readouterr()
+    assert captured.out == 'The number is positive.\n'
+
+def test_condition_2(capsys, monkeypatch):
+    inputs = iter(['yes', 'n', 'y', 'no'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main_condition()
+    captured = capsys.readouterr()
+    assert captured.out == 'The number is positive.\n'
+
+
+def test_infinite_1(capsys, monkeypatch):
+    inputs = iter(['no'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main_infinite()
+    captured = capsys.readouterr()
+    assert captured.out == 'The number is positive.\n'
+
+
+def test_infinite_2(capsys, monkeypatch):
+    inputs = iter(['yes', 'n', 'y', 'no'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    main_infinite()
+    captured = capsys.readouterr()
+    assert captured.out == 'The number is positive.\n'
