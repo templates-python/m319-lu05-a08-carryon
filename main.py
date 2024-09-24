a11d4534f2c1dbbc99dcdd79182a51725b235bbf
graphics80 <kevin.maurizi@bzz.ch>
2024-09-24 14:56:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main_condition():
     # Write your program with a condition here and remove the line "pass"
-    pass
+    while condition:
+        pass
 
 
 def main_infinite():
     # Write your program with break here and remove the line "pass"
-    pass
+    while True:
+        # use break here
+        pass
+        
 
 
 if __name__ == '__main__':
